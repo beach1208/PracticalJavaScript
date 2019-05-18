e3a7e20d6555786a4b85e61250929630a01bc87a
Nagisa Kojima <nk.bg.1208@gmail.com>
2019-05-17 22:54:34 -0700

    Organized object with function

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-let todos = ['item1','item2','item3'];
+let toDoList = {
+    todos: [],
+    displayTodos: function() {
+        console.log("My todos:",this.todos);
+    },
+    addTodo: function(todoText){
+        this.todos.push({
+            todoText: todoText,
+            // set Object propery name as todoText then set value as function parameter todoText
+            completed: false
+        });
+        this.displayTodos();
+    },
+    changeTodos: function(position,todoText){
+        // this.todos[position] = newValue;
+        // Current todoText prperty get and set new todoText
+        this.todos[position].todoText = todoText;
+        this.displayTodos();
+    },
+    deleteTodos: function(position){
+        this.todos.splice(position,1);
+        this.displayTodos();
+    },
+    toggleCompleted: function(position){
+        let todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();
+    }
+};
+
 
 // It should have a function to display todos
 function displayTodos(){
