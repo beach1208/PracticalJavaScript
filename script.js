e9ba889df283c5d748cdd2d0466237a3968751fe
Nagisa Kojima <nk.bg.1208@gmail.com>
2019-08-24 11:03:41 -0700

    dom elements for todos added

--- script.js
+++ script.js
@@ -1,50 +1,25 @@
 let toDoList = {
     todos: [],
-    displayTodos: function() {
-    //    if there are no todos, console.log("Your todo list is empty")
-    if(this.todos.length === 0){
-        console.log("Your todos is empty");
-    }
-    //  else print todos as normal
-      console.log("My todos:");
-        for(let i = 0; i < this.todos.length; i++){
-
-            // check if .completed is true print with (x)
-            if(this.todos[i].completed === true){
-                  console.log('(x)',this.todos[i].todoText);
-            }else {
-                // else print with ()
-                  console.log('( )',this.todos[i].todoText);
-            }
-            
-        }
-
-    },
     addTodo: function(todoText){
         this.todos.push({
             todoText: todoText,
             // set Object propery name as todoText then set value as function parameter todoText
             completed: false
         });
-        this.displayTodos();
     },
     changeTodos: function(position,todoText){
         // this.todos[position] = newValue;
         // Current todoText prperty get and set new todoText
         this.todos[position].todoText = todoText;
-        this.displayTodos();
     },
     deleteTodos: function(position){
         this.todos.splice(position,1);
-        this.displayTodos();
     },
     toggleCompleted: function(position){
         let todo = this.todos[position];  
         // grabbing and referencing specific todo
         todo.completed = !todo.completed;
         // first get true or false of completed and change to the opposite of it
-
-        this.displayTodos();
     },
     toggleAll: function() {
         let totalTodos = this.todos.length;
@@ -67,35 +42,56 @@ let toDoList = {
                 this.todos[i].completed = true;
             }
         }
-        this.displayTodos();
     }
 };
 
 
 const handlers = {
-    displayTodos: function(){
-        toDoList.displayTodos();
-    },
     addTodo: function(){
         let addTodoTextInput = document.getElementById('addTodoTextInput')
         toDoList.addTodo(addTodoTextInput.value);
         addTodoTextInput.value = '';
+        view.displayTodos();
     },
     changeTodo: function(){
         let changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
         let changeTodoTextInput = document.getElementById('changeTodoTextInput');
         toDoList.changeTodos(changeTodoPositionInput.valueAsNumber,changeTodoTextInput.value);
         changeTodoTextInput.value = '';
+        view.displayTodos();
     },
     deleteTodo: function(){
         let deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
         toDoList.deleteTodos(deleteTodoPositionInput.valueAsNumber);
+        view.displayTodos();
     },
     toggleCompleted: function(){
         let toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
         toDoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+        view.displayTodos();
     },
     toggleAll: function() {
         toDoList.toggleAll();
+        view.displayTodos();
     }
 }
+
+var view = {
+    displayTodos: function() {
+         var todosUl = document.querySelector('ul');
+         todosUl.innerHTML = '';
+        for(var i = 0; i < toDoList.todos.length; i++){
+            var todoLi = document.createElement('li');
+            var todo = toDoList.todos[i];
+            
+            var todoTextWithCompletion = '';
+            if(todo.completed === true) {
+                todoTextWithCompletion = '(x) ' + todo.todoText;
+            } else {
+                todoTextWithCompletion = '( ) ' + todo.todoText;
+            }
+            todoLi.textContent = todoTextWithCompletion;
+            todosUl.appendChild(todoLi);
+        }
+    }
+};
